6e7b2cc83bc3927bbda7928ca77bfa59b6861dc7
Sagar Singuluri <sagarsinguluri2005@gmail.com>
2026-08-03 11:06:48 +0530

    Cleanup temporary deployment code

--- backend/problems/urls.py
+++ backend/problems/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from .views import ProblemListCreateView,ProblemDetailView,TestCaseListCreateView,TestCaseDetailView
 from .views import BoilerplateListView
-from .views import seed_database
-from .views import create_admin
 
 urlpatterns = [
     path("", ProblemListCreateView.as_view(), name="problem-list"),
-    path("seed/", seed_database, name="seed"),
     path("<int:pk>/", ProblemDetailView.as_view(), name="problem-detail"),
     path("testcases/", TestCaseListCreateView.as_view()),
     path("testcases/<int:pk>/", TestCaseDetailView.as_view()),
-    path("<int:pk>/boilerplates/", BoilerplateListView.as_view(), name="problem-boilerplates"),
-    path("create-admin/", create_admin),
+    path(
+        "<int:pk>/boilerplates/",
+        BoilerplateListView.as_view(),
+        name="problem-boilerplates",
+    ),
 ]
--- backend/problems/views.py
+++ backend/problems/views.py
@@ -1,49 +1,18 @@
 from rest_framework import generics
-from .models import Problem, TestCase
-from .serializers import ProblemSerializer, TestCaseSerializer
-from rest_framework.permissions import IsAdminUser, AllowAny
-from rest_framework.views import APIView
+from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from .models import Boilerplate
-from .serializers import BoilerplateSerializer
+from rest_framework.views import APIView
 from rest_framework.filters import SearchFilter, OrderingFilter
+
 from django_filters.rest_framework import DjangoFilterBackend
-from django.http import JsonResponse
-from django.contrib.auth import get_user_model
-from django.conf import settings
-from pathlib import Path
-from django.http import JsonResponse
-from django.contrib.auth import get_user_model
 
-def create_admin(request):
-    User = get_user_model()
+from .models import Problem, TestCase, Boilerplate
+from .serializers import (
+    ProblemSerializer,
+    TestCaseSerializer,
+    BoilerplateSerializer,
+)
 
-    if User.objects.filter(username="admin").exists():
-        return JsonResponse({"message": "Admin already exists"})
-
-    User.objects.create_superuser(
-        username="admin",
-        email="admin@example.com",
-        password="Admin@123"
-    )
-
-    return JsonResponse({"message": "Admin created successfully"})
-
-def seed_database(request):
-    User = get_user_model()
-
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@example.com",
-            password="Admin@123"
-        )
-
-    seed_file = Path(settings.BASE_DIR) / "seed_problems.py"
-    exec(seed_file.read_text(encoding="utf-8"), {})
-
-    return JsonResponse({"status": "success"})
 
 
 class BoilerplateListView(APIView):
@@ -55,10 +24,8 @@ class BoilerplateListView(APIView):
         return Response(serializer.data)
 
 class ProblemListCreateView(generics.ListCreateAPIView):
-
     queryset = Problem.objects.all()
     serializer_class = ProblemSerializer
-
     pagination_class = None
 
     filter_backends = [
@@ -81,6 +48,8 @@ class ProblemListCreateView(generics.ListCreateAPIView):
     ]
 
     ordering = ["title"]
+
+
 class ProblemDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Problem.objects.all()
     serializer_class = ProblemSerializer
